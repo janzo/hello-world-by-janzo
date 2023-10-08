c666b781192acc050a6ce7dc947ac4bc9778936d
janzo <gllenza@yahoo.it>
2023-10-08 16:38:21 +0200

    code cleaning

--- src/components/CountriesTable.vue
+++ src/components/CountriesTable.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="jnzwrap">
       <b-row>
-        <!-- offset-md="4" -->
           <b-col md="4" class=" d-flex justify-content-center align-items-center">
               <b-form-input ref="filter" v-model="filter" placeholder="Search..." class="mr-2"></b-form-input>
               <b-icon icon="search" size="sm"></b-icon>
@@ -18,7 +17,6 @@
       <b-row>
         <b-col>
         <div class="table-responsive">
-          
           <b-table class="my-table"
           :items="countryRows" 
           :fields="tableFields" 
@@ -31,10 +29,6 @@
           :per-page="perPage"
           :current-page.sync="currentPage" 
           >
-            <!-- <template #cell(flag)="fla"> -->
-                <!-- <img :src="fla.item.flags.png" :id="'id-'+fla.item.country_code" height="30"> -->
-                <!-- <b-tooltip :target="'id-' + fla.item.country_code" :title="fla.item.flags.alt"></b-tooltip> -->
-
             <template #cell(flag)="fla">
               <div @mouseover="showTooltip(fla.item.country_code)" @mouseleave="hideTooltip(fla.item.country_code)">
                 <img :src="fla.item.flags.png" :id="'id-' + fla.item.country_code" height="30">
@@ -47,11 +41,7 @@
               </div>
             </template>
 
-
-            <!-- </template> -->
-            <!-- <b-tooltip v-for = "c in countries" :key="c.country_code" :target="'id-'+c.country_code" :title="c.flags.alt"></b-tooltip> -->
           </b-table>
-          <!-- Aggiungi il componente di paginazione -->
           <b-pagination
             class="custom-pagination d-flex justify-content-center w-100"
             v-model="currentPage"
@@ -62,24 +52,13 @@
           </div>
          </b-col>
       </b-row>
-      
-      <!-- <vue-json-pretty :data="countryRows"></vue-json-pretty> -->
-      <!-- <vue-json-pretty :data="countries"></vue-json-pretty> -->
-      <!-- {{ countryRows }} -->
-
-      
     </div>
   </template>
   
   <script>
-  // import VueJsonPretty from 'vue-json-pretty';
-  // import 'vue-json-pretty/lib/styles.css';
-  // import { BTooltip } from 'bootstrap-vue';
   export default {
     name: 'CountriesTable',
     components:{
-        //  VueJsonPretty,
-        // BTooltip,
     },
     props: {
       countries: {
@@ -97,10 +76,7 @@
     };
   },
     computed: {
-      // Calcola i campi da visualizzare nella tabella
       tableFields() {
-
-        // Utilizza il primo elemento dell'array per determinare i campi
         const sampleCountry = this.countries[0];
         if (!sampleCountry) {
           return [];
@@ -147,10 +123,8 @@
           sortable: true, 
         },
       ];
-        // return Object.keys(sampleCountry);
 
       },
-      // Calcola le righe dei Paesi da visualizzare nella tabella
       countryRows() {
         return this.countries.map(country => ({
                 // ...country,
@@ -162,85 +136,56 @@
                 capital: country.capital && country.capital[0] ? country.capital[0] : '',
                 population: country.population  || '',
                 flags: country.flags  || '',
-                // Aggiungi eventuali calcoli o formattazioni qui
         }));
       },
-      isSmallScreen() {
-      // const currentBreakpoint = this.$bvBreakpoints.name;
-      // return currentBreakpoint === 'sm';
-      return false;
-    },
     },
     methods: {
-  showTooltip(countryCode) {
-    document.getElementById('tooltip-' + countryCode).style.display = 'block';
-  },
-  hideTooltip(countryCode) {
-    document.getElementById('tooltip-' + countryCode).style.display = 'none';
-  },
-  getAlt(flags) {
-  let tooltipContent = '';
-  let wrap = flags.alt && (flags.svg || flags.png)
+      showTooltip(countryCode) {
+        document.getElementById('tooltip-' + countryCode).style.display = 'block';
+      },
+      hideTooltip(countryCode) {
+        document.getElementById('tooltip-' + countryCode).style.display = 'none';
+      },
+      getAlt(flags) {
+        let tooltipContent = '';
+        let wrap = flags.alt && (flags.svg || flags.png)
+        if( wrap ){
+          tooltipContent += '<div class="row">';
+        }
 
-  // Colonna per l'immagine
+        if (flags.svg || flags.png) {  
+          if( wrap ) tooltipContent += '<div class="col-md-6">';
+          if (flags.svg) {
+            tooltipContent += `<img src="${flags.svg}" class="img-fluid img-flag" />`;
+          } else if (flags.png) {
+            tooltipContent += `<img src="${flags.png}" class="img-fluid img-flag"/>`;
+          }
+          if( wrap ) tooltipContent += '</div>'; 
+        }
+      
+        if (flags.alt) {
+          if( wrap ) tooltipContent += '<div class="col-md-6">';
+          tooltipContent += flags.alt;
+          if( wrap ) tooltipContent += '</div>'; 
+        }
+        if(wrap){
+          tooltipContent += '</div>';
+        }
+        return tooltipContent;
+        },
 
-  if( wrap ){
-    tooltipContent += '<div class="row">';
-  }
-
-  if (flags.svg || flags.png) {  
-    if( wrap ) tooltipContent += '<div class="col-md-6">';
-    if (flags.svg) {
-      // Se esiste un'immagine SVG, la visualizziamo
-      tooltipContent += `<img src="${flags.svg}" class="img-fluid img-flag" />`;
-    } else if (flags.png) {
-      // Se esiste un'immagine PNG, la visualizziamo
-      tooltipContent += `<img src="${flags.png}" class="img-fluid img-flag"/>`;
-    }
-    if( wrap ) tooltipContent += '</div>'; // Chiudi la colonna dell'immagine
-  }
-
-  
-  if (flags.alt) {
-    // Colonna per la descrizione
-    if( wrap ) tooltipContent += '<div class="col-md-6">';
-
-    // Se esiste un testo nell'attributo alt, lo visualizziamo
-    tooltipContent += flags.alt;
-
-
-    if( wrap ) tooltipContent += '</div>'; // Chiudi la colonna della descrizione
-  }
-  if(wrap){
-    tooltipContent += '</div>'; // Chiudi la riga
-  }
-
-  return tooltipContent;
-},
-
-},
+    },
   };
   </script>
   
-  
-
-
   <style scoped>
-  /* .my-table{
-    background-color: antiquewhite;
-  } */
   .custom-tooltip {
-  position: absolute;
-  background: rgba(0, 0, 0, 0.8);
-  color: #fff;
-  padding: 5px;
-  border-radius: 5px;
-  z-index: 1000;
-}
-/* input{
-  background-color: cornsilk;
-} */
-
-
+    position: absolute;
+    background: rgba(0, 0, 0, 0.8);
+    color: #fff;
+    padding: 5px;
+    border-radius: 5px;
+    z-index: 1000;
+  }
   </style>
   
